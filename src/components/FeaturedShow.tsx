f8106d896b9ad6de586f28f0c472f127f904844d
apella1 <johncollons5@gmail.com>
2025-01-31 16:08:41 +0300

    feat: set up redux store

--- src/components/FeaturedShow.tsx
+++ src/components/FeaturedShow.tsx
@@ -1,8 +1,8 @@
 export default function FeaturedShow() {
   return (
-    <section className="h-[80vh] flex flex-col justify-end bg-[url(/images/header1.jpg)] p-16 text-white font-medium bg-cover">
-      <div className="flex flex-col space-y-3 text-sm">
-        <h1>Gladiator II</h1>
+    <section className="h-[80vh] flex flex-col justify-end bg-[url(/images/header2.jpg)] p-16 text-white font-medium bg-cover">
+      <div className="flex flex-col space-y-3 text-base">
+        <h1 className="font-semibold">Gladiator II</h1>
         <div className="flex items-center space-x-3">
           <p>HD</p>
           <p>7.1</p>
@@ -19,7 +19,7 @@ export default function FeaturedShow() {
           inventore facere harum! Magni atque possimus quas corrupti
           laudantium...
         </p>
-        <button className="self-start bg-blue-500 text-white px-4 py-1.5 rounded-lg">
+        <button className="self-start bg-blue-500 text-white px-4 py-1.5 rounded-lg text-sm">
           Watch Now
         </button>
       </div>
--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -2,7 +2,7 @@ import Navbar from "../components/ui/navbar";
 
 export default function Movies() {
   return (
-    <div className="p-8">
+    <div className="">
       <Navbar />
     </div>
   );
